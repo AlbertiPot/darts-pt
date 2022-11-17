aab2b1c5ef423fc1e77721aadc3fb44667396000
AlbertiPot <1071794833@qq.com>
2022-11-17 18:58:29 +0800

    ablate_2loss

--- exp_scripts/darts-proj-sota.sh
+++ exp_scripts/darts-proj-sota.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-script_name=`basename "$0"`
-id=${script_name%.*}
+script_name=`basename "$0"` # 反引号(也可以用"$()")命令替换：执行反引号中的命令，basename返回文件的名字（去掉目录名)
+id=${script_name%.*}    # %表明对script_name变量截取左边的，#号是截取右边的，这里是截取从右往左数第一个碰到点左面的
 dataset=${dataset:-cifar10}
 seed=${seed:-2}
 gpu=${gpu:-"auto"}
@@ -11,10 +11,11 @@ resume_epoch=${resume_epoch:-50}
 resume_expid=${resume_expid:-'search-darts-sota-s5-2'}
 crit=${crit:-'acc'}
 
-while [ $# -gt 0 ]; do
+# 这里读取 --resume_expid search-darts-sota-s5-2，对第一个变量去掉--，申明一个 名为resume_expid的变量，幅值为search-darts-sota-s5-2
+while [ $# -gt 0 ]; do  # 大于greater than >
     if [[ $1 == *"--"* ]]; then
-        param="${1/--/}"
-        declare $param="$2"
+        param="${1/--/}"    # / / 中间是删除的变量
+        declare $param="$2"     # 申明变量， 变量名为param
         # echo $1 $2 // Optional to see the parameter:value result
     fi
     shift
